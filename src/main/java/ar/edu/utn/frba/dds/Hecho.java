ff8cc2cfa83df5c3610deb89e23d245661ae4b54
Constanza Serra <constanza_serra@hotmail.com>
2025-05-05 23:03:42 -0300

    Primer commit de prueba

---
+++ src/main/java/ar/edu/utn/frba/dds/Hecho.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.frba.dds;
+
+import java.time.LocalDate;
+
+public class Hecho {
+  private String titulo;
+  private String descripcion;
+  private String categoria;
+  private String contenidoMultimedia;
+  private Double latitud;
+  private Double longitud;
+  private LocalDate fechaDelHecho;
+  private OrigenHecho origen;
+
+
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/OrigenHecho.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds;
+
+public enum OrigenHecho {
+  ESTATICA,
+  DINAMICA,
+  INTERMEDIA
+}
