8e21b90024a3c256a17abcbedec2ba9184b447cd
Facundo Malnero <fmalnero@frba.utn.edu.ar>
2025-06-03 20:52:29 -0300

    feat: interfaz detector de spam

--- src/main/java/ar/edu/utn/frba/dds/repositories/fuentes/FuenteIntermedia.java
+++ src/main/java/ar/edu/utn/frba/dds/repositories/fuentes/FuenteIntermedia.java
@@ -4,7 +4,7 @@ import ar.edu.utn.frba.dds.models.Hecho;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FuenteIntermedia implements Fuente{
+public class FuenteIntermedia implements Fuente {
   private List<Hecho> listaVacia = new ArrayList<>();
 
   public List<Hecho> obtenerHechos() {
