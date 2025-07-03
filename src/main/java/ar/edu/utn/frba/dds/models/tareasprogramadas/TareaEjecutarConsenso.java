e3ef5eeb9c8b3b3599e0918685fda8999f4cb820
Constanza Serra <aserra@frba.utn.edu.ar>
2025-07-03 01:40:30 -0300

    feat + test: Se agrega algoritmo de Consenso Absoluto y sus tests

--- src/main/java/ar/edu/utn/frba/dds/models/tareasprogramadas/TareaEjecutarConsenso.java
+++ src/main/java/ar/edu/utn/frba/dds/models/tareasprogramadas/TareaEjecutarConsenso.java
@@ -4,12 +4,12 @@ import ar.edu.utn.frba.dds.contracts.TareaProgramada;
 import ar.edu.utn.frba.dds.models.Coleccion;
 
 public class TareaEjecutarConsenso implements TareaProgramada {
-  private Coleccion coleccion;
-
-  public TareaEjecutarConsenso(Coleccion coleccion) {
-    this.coleccion = coleccion;
-  }
-
+  //private Coleccion coleccion;
+  //
+  //  public TareaEjecutarConsenso(Coleccion coleccion) {
+  //    this.coleccion = coleccion;
+  //  }
+  //comento porque se considera bug
   @Override
 
   public void ejecutar() {
