68ebb0193550a65a5abb600b780ce518483b1a56
FacundoMalnero <102746561+FacundoMalnero@users.noreply.github.com>
2025-07-03 23:23:00 -0300

    feat: repositorio de hechos (#22)

---
+++ src/main/java/ar/edu/utn/frba/dds/repositories/HechosRepository.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.frba.dds.repositories;
+
+import ar.edu.utn.frba.dds.models.Hecho;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class HechosRepository {
+  private static final HechosRepository INSTANCE = new HechosRepository();
+  private final List<Hecho> hechos = new ArrayList<>();
+
+  private HechosRepository() {}
+
+  public static HechosRepository getInstance() {
+    return INSTANCE;
+  }
+
+  public List<Hecho> getHechos() {
+    return Collections.unmodifiableList(hechos);
+  }
+
+  public void agregarHecho(Hecho hecho) {
+    hechos.add(hecho);
+  }
+}
